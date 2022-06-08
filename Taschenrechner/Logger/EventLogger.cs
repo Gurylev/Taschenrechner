669bdbc8c5be23756b5f9cdf2bc0a00e4ccd9c6f
Gurylev.oleg@gmail.com <Gurylev.oleg@gmail.com>
2022-06-08 14:43:18 +0300

    Добавьте файлы проекта.

---
+++ Taschenrechner/Logger/EventLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Taschenrechner.Logger
+{
+    public class EventLogger : IDisposable
+    {
+        public EventLogger(string level, string message)
+        {           
+            LogManager.Setup().LoadConfiguration(builder => {
+                builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "${basedir}/logs/${shortdate}.log", "${longdate} ${level} ${message} ${exception} ");
+            });
+            var logger = LogManager.GetCurrentClassLogger();
+
+            switch (level)
+            {
+                case "Info":
+                    logger.Info(message);
+                    break;
+                case "Debug":
+                    logger.Debug(message);
+                    break;
+                default:    
+                    logger.Error(message);
+                    break;
+            }            
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Taschenrechner/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Taschenrechner.Logger;
+
+namespace Taschenrechner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (_ = new EventLogger("Info", "Приложение запущено!")) ;
+            #region Инициализация всех кнопок
+            foreach (UIElement el in Normal.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+
+            //foreach (UIElement el2 in Engineer.Children)
+            //{
+            //    if (el2 is Button)
+            //    {
+            //        ((Button)el2).Click += Button2_Click;
+            //    }
+            //}
+            #endregion
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+                textLabel.Text = "";
+            else if (str == "=")
+            {
+                try
+                {
+                    string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                    textLabel.Text = value;
+                }
+                catch (Exception ex)
+                {
+                    using (_ = new EventLogger("Debug", ex.ToString()));
+                }
+            }
+            else
+                textLabel.Text += str;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //private void Button2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string str = (string)((Button)e.OriginalSource).Content;
+
+        //    if (str == "AC")
+        //        textLabel2.Text = "";
+        //    else if (str == "=")
+        //    {
+        //        string value = new DataTable().Compute(textLabel2.Text, null).ToString();
+        //        textLabel2.Text = value;
+        //    }
+        //    else
+        //        textLabel2.Text += str;
+        //}
+    }
+}
